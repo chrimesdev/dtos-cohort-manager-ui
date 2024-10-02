1805c7b700b2ffe2e0904ae2cf614825c9fa9330
Adam Chrimes <iChrimes@live.co.uk>
2024-10-02 16:50:26 +0100

    add a basic 404 not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "We cannot find the page you’re looking for - Cohort Manager",
+};
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="nhsuk-width-container">
+        <main className="nhsuk-main-wrapper" id="maincontent" role="main">
+          <div className="nhsuk-grid-row">
+            <div className="nhsuk-grid-column-two-thirds">
+              <h1>We cannot find the page you’re looking for</h1>
+              <p>
+                <Link href="/">Return to the homepage</Link>
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
