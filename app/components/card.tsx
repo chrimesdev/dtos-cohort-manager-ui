22a2a2c05efecf68d537f206d27f91854d0704ae
Adam Chrimes <iChrimes@live.co.uk>
2024-11-13 17:37:07 +0000

    remove Link legacy behaviour from the card component

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -13,10 +13,11 @@ export default function Card({ value, label, url }: CardProps) {
         <p className="nhsuk-heading-xl nhsuk-u-font-size-64 nhsuk-u-margin-bottom-1">
           {value} <span className="nhsuk-u-visually-hidden">{label}</span>
         </p>
-        <Link href={url} legacyBehavior>
-          <a className="nhsuk-card__link nhsuk-u-font-weight-normal nhsuk-u-font-size-19 nhsuk-link--no-visited-state">
-            {label}
-          </a>
+        <Link
+          href={url}
+          className="nhsuk-card__link nhsuk-u-font-weight-normal nhsuk-u-font-size-19 nhsuk-link--no-visited-state"
+        >
+          {label}
         </Link>
       </div>
     </div>
