2732f9e61e591a3f0a2bf168366b65c16b2056ad
Adam Chrimes <iChrimes@live.co.uk>
2024-11-13 18:52:51 +0000

    tidy up the tests

--- e2e/navigation.test.ts
+++ e2e/navigation.test.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
 test("navigate to the Overview page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page).toHaveTitle(/Overview/);
+  await page.goto("/");
+  await expect(page.getByRole("heading", { name: "Overview" })).toBeVisible();
 });
 
 test("navigate from Overview to Exceptions summary", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
   await page.getByRole("link", { name: "Breast screening exceptions" }).click();
   await expect(
     page.getByRole("heading", { name: "Exceptions summary" })
@@ -16,9 +16,9 @@ test("navigate from Overview to Exceptions summary", async ({ page }) => {
 test("navigate from Exceptions summary to Participant information", async ({
   page,
 }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
   await page.getByRole("link", { name: "Breast screening exceptions" }).click();
-  await page.getByRole("link", { name: "723 333 6897" }).click();
+  await page.getByRole("link", { name: "10001767" }).click();
   await expect(
     page.getByRole("heading", { name: "Participant information" })
   ).toBeVisible();
