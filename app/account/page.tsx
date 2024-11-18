f9b4b805be665a7919f7668adfa5c2b1b03ab2ba
Adam Chrimes <iChrimes@live.co.uk>
2024-11-18 09:39:56 +0000

    use earlier version of Next until its stable

---
+++ app/account/page.tsx
@@ -0,0 +1,66 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import SignIn from "@/app/components/signIn";
+import { auth } from "@/app/lib/auth";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const session = await auth();
+
+  if (session?.user) {
+    return {
+      title: "Account - NHS England",
+    };
+  }
+
+  return {
+    title: "Log in with your Care Identity account - NHS England",
+  };
+}
+
+export default async function Page() {
+  const serviceName = process.env.SERVICE_NAME;
+  const session = await auth();
+
+  return (
+    <>
+      <main className="nhsuk-main-wrapper" id="maincontent" role="main">
+        <div className="nhsuk-grid-row">
+          <div className="nhsuk-grid-column-two-thirds">
+            {session?.user ? (
+              <>
+                <h1>Account</h1>
+                <dl className="nhsuk-summary-list">
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">Name</dt>
+                    <dd className="nhsuk-summary-list__value">
+                      {session?.user?.firstName} {session?.user?.lastName}
+                    </dd>
+                  </div>
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">Organisation</dt>
+                    <dd className="nhsuk-summary-list__value">
+                      {session?.user?.orgName} ({session?.user?.odsCode})
+                    </dd>
+                  </div>
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">Roles</dt>
+                    <dd className="nhsuk-summary-list__value">
+                      {session?.user?.roles}
+                    </dd>
+                  </div>
+                </dl>
+                <p>
+                  <Link href="/">Go back to the Overview page</Link>
+                </p>
+              </>
+            ) : (
+              <>
+                <SignIn serviceName={serviceName} />
+              </>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
