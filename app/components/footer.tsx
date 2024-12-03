6e277ecf3f25b1fe1e7d93515788c72169b191ff
Adam Chrimes <iChrimes@live.co.uk>
2024-12-03 11:51:25 +0000

    add cookie policy and accessibility statement pages

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,35 +1,55 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <>
       <footer role="contentinfo">
         <div className="nhsuk-footer-container">
           <div className="nhsuk-width-container">
-            <h2 className="nhsuk-u-visually-hidden">Support links</h2>
-            <div className="nhsuk-footer">
-              <ul className="nhsuk-footer__list">
-                <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
-                  <a className="nhsuk-footer__list-item-link" href="#">
-                    Accessibility statement
-                  </a>
-                </li>
-                <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
-                  <a className="nhsuk-footer__list-item-link" href="#">
-                    Cookies
-                  </a>
-                </li>
-                <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
-                  <a className="nhsuk-footer__list-item-link" href="#">
-                    Privacy policy
-                  </a>
-                </li>
-                <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
-                  <a className="nhsuk-footer__list-item-link" href="#">
-                    Terms and conditions
-                  </a>
-                </li>
-              </ul>
-              <div>
-                <p className="nhsuk-footer__copyright">&copy; NHS England</p>
+            <div className="nhsuk-grid-row">
+              <div className="nhsuk-grid-column-full">
+                <p className="nhsuk-u-secondary-text-color nhsuk-u-font-size-16">
+                  This website only stores the cookies that are needed to make
+                  it work. Read more about how{" "}
+                  <Link
+                    href="/cookies-policy"
+                    className="nhsuk-footer__list-item-link"
+                  >
+                    we use cookies.
+                  </Link>
+                </p>
+              </div>
+            </div>
+            <div className="nhsuk-grid-row">
+              <div className="nhsuk-grid-column-full">
+                <h2 className="nhsuk-u-visually-hidden">Support links</h2>
+                <div className="nhsuk-footer">
+                  <ul className="nhsuk-footer__list">
+                    <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
+                      <Link
+                        className="nhsuk-footer__list-item-link"
+                        href="/accessibility-statement"
+                      >
+                        Accessibility statement
+                      </Link>
+                    </li>
+                    <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
+                      <a className="nhsuk-footer__list-item-link" href="#">
+                        Privacy policy
+                      </a>
+                    </li>
+                    <li className="nhsuk-footer__list-item nhsuk-footer-default__list-item">
+                      <a className="nhsuk-footer__list-item-link" href="#">
+                        Terms and conditions
+                      </a>
+                    </li>
+                  </ul>
+                  <div>
+                    <p className="nhsuk-footer__copyright">
+                      &copy; NHS England
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
